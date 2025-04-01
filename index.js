ce3221123a5e47e59ced224702b69c2680ee9d5b
Yampoy <yampoy.elvis@gmail.com>
2025-04-01 11:41:52 +0300

    done

--- index.js
+++ index.js
@@ -1,23 +1,84 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+// Creates an employee record from an array
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
 }
 
+// Creates multiple employee records from an array of arrays
+function createEmployeeRecords(employeesData) {
+    return employeesData.map(createEmployeeRecord);
+}
+
+// Creates a time in event for an employee
+function createTimeInEvent(employee, dateTime) {
+    if (!dateTime || typeof dateTime !== 'string') {
+        throw new Error("Invalid date/time format");
+    }
+    
+    const [date, hour] = dateTime.split(' ');
+    
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    });
+    
+    return employee;
+}
+
+// Creates a time out event for an employee
+function createTimeOutEvent(employee, dateTime) {
+    if (!dateTime || typeof dateTime !== 'string') {
+        throw new Error("Invalid date/time format");
+    }
+    
+    const [date, hour] = dateTime.split(' ');
+    
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    });
+    
+    return employee;
+}
+
+// Calculates hours worked on a specific date
+function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date);
+    const timeOut = employee.timeOutEvents.find(event => event.date === date);
+    
+    if (!timeIn || !timeOut) {
+        throw new Error("Missing time in/out event for date");
+    }
+    
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+// Calculates wages earned on a specific date
+function wagesEarnedOnDate(employee, date) {
+    const hours = hoursWorkedOnDate(employee, date);
+    return hours * employee.payPerHour;
+}
+
+// Calculates all wages for an employee
+function allWagesFor(employee) {
+    const dates = employee.timeInEvents.map(event => event.date);
+    return dates.reduce((total, date) => total + wagesEarnedOnDate(employee, date), 0);
+}
+
+// Finds an employee by first name
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+// Calculates payroll for all employees
+function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+}
